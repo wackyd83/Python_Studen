21ce8404db05c1ebc5d8e9d1ad047b41eb5dad4f
wackyd-t14 <wackyd83@gmail.com>
2023-02-17 19:44:10 +0800

    使用序列化器进行参数校验： 1.er.is_valid(raise_exception=True) 2.自定义序列化器时，给字段添加限制： 参数名称	作用 max_length	最大长度 min_lenght	最小长度 allow_blank	是否允许为空 trim_whitespace	是否截断空白字符 max_value	最小值 min_value	最大值
    参数名称	说明
    read_only	表明该字段仅用于序列化输出，默认False
    write_only	表明该字段仅用于反序列化输入，默认False
    required	表明该字段在反序列化时必须输入，默认True
    default	反序列化时使用的默认值
    allow_null	表明该字段是否允许传入None，默认False
    validators	该字段使用的验证器
    error_messages	包含错误编号与错误信息的字典
    label	用于HTML展示API页面时，显示的字段名称
    help_text	用于HTML展示API页面时，显示的字段帮助提示信息

--- django-project/DRF_demo/book_drf/serializer.py
+++ django-project/DRF_demo/book_drf/serializer.py
@@ -4,20 +4,21 @@ from rest_framework import serializers
 # 自定义序列化器
 class HeroInfoSerializer(serializers.Serializer):
     '''英雄序列化器'''
-    hname=serializers.CharField()
-    hcomment=serializers.CharField()
+    hname = serializers.CharField()
+    hcomment = serializers.CharField()
 
     # 多对一
-
     # hbook=serializers.PrimaryKeyRelatedField(read_only=True)
-    hbook=serializers.StringRelatedField()
+    hbook = serializers.StringRelatedField()
+
 
 class BookSerializer(serializers.Serializer):
     # 序列化返回字段
     # 根据该字段的模型类定义时的数据类型进行定义
-    btitle = serializers.CharField()
-    bpub_date = serializers.DateField()
-    bread = serializers.IntegerField()
+    btitle = serializers.CharField(max_length=20, min_length=5)
+    bpub_date = serializers.DateField()  # required=False:把传递的参数设置为可选
+    bread = serializers.IntegerField(max_value=100, min_value=5)
+    bcomment = serializers.DateField(default=10)
 
     # # 返回关联的英雄id:PrimaryKeyRelatedField
     # heroinfo_set = serializers.PrimaryKeyRelatedField(read_only=True, many=True)
@@ -27,6 +28,4 @@ class BookSerializer(serializers.Serializer):
 
     # 一对多
     # 返回多个字段，需要使用序列化嵌套
-    heroinfo_set=HeroInfoSerializer(many=True)
-
-
+    # heroinfo_set=HeroInfoSerializer(many=True)
--- django-project/DRF_demo/book_drf/views.py
+++ django-project/DRF_demo/book_drf/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django import http
 from django.shortcuts import render
 from django.views import View
@@ -16,6 +18,27 @@ class Books(View):
 
         return http.JsonResponse(ser.data,safe=False)
 
+    def post(self, request):
+        '''保存数据'''
+        # 1.获取参数
+        data=request.body.decode()
+        data_dict=json.loads(data)
+
+        # 2.校验参数
+        ser=BookSerializer(data=data_dict)
+        ser.is_valid(raise_exception=True)  # 序列化器的参数验证方法,raise_exception=True:自动抛出异常进行返回
+
+        # 3.保存数据
+        # book = BookInfo.objects.create(
+        #     btitle=btitle,
+        #     bpub_date=bpub_date,
+        #     bcomment=bcomment,
+        #     bread=bread
+        # )
+
+        # 4.返回结果
+        return http.JsonResponse(ser.errors)
+
 class Book(View):
 
     def get(self,request,pk):
@@ -27,5 +50,4 @@ class Book(View):
         ser=BookSerializer(book)  # 序列化单个对象时，无需加参数many=True
 
         return http.JsonResponse(ser.data, safe=False)
-
 
