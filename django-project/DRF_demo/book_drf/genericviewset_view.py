b615c8a129de0d389f4d75212142ccd9c1bdd097
wackyd-t14 <wackyd83@gmail.com>
2023-02-20 16:08:48 +0800

    GenericSet视图集的基本使用： 1.使用GenericSet视图集时（跟GenericAPIView的使用方法一样）:   。指定当前类视图使用的查询集数据   。指定当前视图使用的序列化器

--- django-project/DRF_demo/book_drf/genericviewset_view.py
+++ django-project/DRF_demo/book_drf/genericviewset_view.py
@@ -1,6 +1,6 @@
-# ViewSet视图集的使用
+# GenericSet视图集的使用
 
-from rest_framework.viewsets import ViewSet
+from rest_framework.viewsets import GenericViewSet
 # GenericAPIView继承自APIView，并且增加了几个特性:分页、过滤排序
 from rest_framework.response import Response  # rest_framework框架自带的response类
 
@@ -10,14 +10,17 @@ from books.models import BookInfo
 
 # Create your views here.
 
-class Books(ViewSet):
-    # 定义视图类时，如果继承自GenericAPIView，request和response就要使用rest_framework的类
-    def list(self, request):
-        query_params = request.query_params
-        # 1.查询所有图书对象
-        books = BookInfo.objects.all()
+class Books(GenericViewSet):
 
-        ser = BookSerializer(books, many=True)  # 序列化多个对象时，需要加参数many=True
+    queryset = BookInfo.objects.all()  # 指定当前类视图使用的查询集数据
+    serializer_class = BookSerializer  # 指定当前视图使用的序列化器
+
+
+    def list(self, request):
+        # 1.查询所有图书对象
+        books = self.get_queryset()
+
+        ser = self.get_serializer(books,many=True)  # 序列化多个对象时，需要加参数many=True
 
         return Response(ser.data)
 
@@ -28,7 +31,7 @@ class Books(ViewSet):
         data = request.data
 
         # 2.校验参数
-        ser = BookSerializer(data=data)
+        ser = self.get_serializer(data=data)
         # ser.is_valid(raise_exception=True)  # 序列化器的参数验证方法,raise_exception=True:自动抛出异常进行返回
         ser.is_valid()  # 序列化器的参数验证方法,raise_exception=True:自动抛出异常进行返回
         # ser.validated_data:参数验证的字段数据
@@ -42,21 +45,24 @@ class Books(ViewSet):
     # 在ListCreateAPIView类中已封装好get和post方法，因此不需要自己写就可以实现对数据批量查询和新增操作
 
 
-class BookDRFView(ViewSet):
+class BookDRFView(GenericViewSet):
+
+    queryset = BookInfo.objects.all()  # 指定当前类视图使用的查询集数据
+    serializer_class = BookSerializer  # 指定当前视图使用的序列化器
 
     '''自定义一个方法名称不是list()、retrieve()、create()、update()、destory()时'''
     def lastdata(self,request,pk):
-        book=BookInfo.objects.get(id=pk)
-        ser=BookSerializer(book)
+        book=self.get_queryset(id=pk)
+        ser=self.get_serializer(book)
         return Response(ser.data)
 
     def get(self, request, pk):
         try:
-            book = BookInfo.objects.get(id=pk)
+            book = self.get_queryset(id=pk)
         except BookInfo.DoesNotExist:
             return Response({'error': '书籍不存在'}, status=400)
 
-        ser = BookSerializer(book)  # 序列化单个对象时，无需加参数many=True
+        ser = self.get_serializer(book)  # 序列化单个对象时，无需加参数many=True
 
         return Response(ser.data)
 
@@ -67,10 +73,10 @@ class BookDRFView(ViewSet):
 
         # 2.验证数据
         try:
-            book = BookInfo.objects.get(id=pk)
+            book = self.get_queryset(id=pk)
         except BookInfo.DoesNotExist:
             return Response({'error': '书籍不存在'}, status=400)
-        ser = BookSerializer(book, data=data)
+        ser = self.get_serializer(book, data=data)
         ser.is_valid()
 
         # 3.更新数据
@@ -83,7 +89,7 @@ class BookDRFView(ViewSet):
         '''删除数据'''
 
         try:
-            book = BookInfo.objects.get(id=pk)
+            book = self.get_queryset(id=pk)
         except BookInfo.DoesNotExist:
             return Response({'error': '书籍不存在'}, status=400)
 
--- django-project/DRF_demo/book_drf/urls.py
+++ django-project/DRF_demo/book_drf/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path,re_path
-from . import views,apiview_view,genericapiview_view,mixin_view,childmixin_view,viewset_view
+from . import views,apiview_view,genericapiview_view,mixin_view,childmixin_view,viewset_view,genericviewset_view
 
 urlpatterns = [
     # re_path(r'^books_drf/$', views.Books.as_view()),
@@ -14,12 +14,19 @@ urlpatterns = [
     # re_path(r'^books_drf/$', childmixin_view.Books.as_view()),
     # re_path(r'^book_drf/(?P<pk>\d+)/$', childmixin_view.BookDRFView.as_view()),
 
-    # ViewSet路由使用
-    # 把请求方法和自定义方法的名称使用字典做对应
-    re_path(r'^books_drf/$', viewset_view.Books.as_view({'get':'list','post':'create'})),
-    re_path(r'^book_drf/(?P<pk>\d+)/$', viewset_view.BookDRFView.as_view({'put':'update',})),
+    # # ViewSet路由使用
+    # # 把请求方法和自定义方法的名称使用字典做对应
+    # re_path(r'^books_drf/$', viewset_view.Books.as_view({'get':'list','post':'create'})),
+    # re_path(r'^book_drf/(?P<pk>\d+)/$', viewset_view.BookDRFView.as_view({'put':'update',})),
+    # # 当使用ViewSet视图集时，自定义方法名称不是list()、retrieve()、create()、update()、destory()这5个时，需要使用以下的路由定义方式：
+    # # 在请求地址后添加自定义名称
+    # re_path(r'^book_drf/(?P<pk>\d+)/lastdata/$', viewset_view.BookDRFView.as_view({'get':'lastdata',})),
 
+    # GenericViewSet路由使用
+    # 把请求方法和自定义方法的名称使用字典做对应
+    re_path(r'^books_drf/$', genericviewset_view.Books.as_view({'get':'list','post':'create'})),
+    re_path(r'^book_drf/(?P<pk>\d+)/$', genericviewset_view.BookDRFView.as_view({'put':'update',})),
     # 当使用ViewSet视图集时，自定义方法名称不是list()、retrieve()、create()、update()、destory()这5个时，需要使用以下的路由定义方式：
     # 在请求地址后添加自定义名称
-    re_path(r'^book_drf/(?P<pk>\d+)/lastdata/$', viewset_view.BookDRFView.as_view({'get':'lastdata',})),
+    re_path(r'^book_drf/(?P<pk>\d+)/lastdata/$', genericviewset_view.BookDRFView.as_view({'get':'lastdata',})),
 ]
