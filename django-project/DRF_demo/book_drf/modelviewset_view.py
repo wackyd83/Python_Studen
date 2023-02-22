f08e3bda46b886d3c9127ab5f40cc345aaac1d3e
wackyd-t14 <wackyd83@gmail.com>
2023-02-22 10:41:17 +0800

    使用action自动生成自定义方法的路由： 1.导入action装饰器： from rest_framework.decorators import action 2.调用action装饰器： @action(methods=['get'], detail=True)  # methods:指定自定义方法对应的请求方法，detail:路由上如果要带pk参数，则使用detail=True将自动生成带pk参数的路由地址
    视图类和视图集的使用场景：
    1.视定义该类的需求决定：如果这个类需要使用多种方法，则使用视图集比较方便；如果这个类只使用1、2种方法，则使用视图类子类更节省资源。

--- django-project/DRF_demo/book_drf/modelviewset_view.py
+++ django-project/DRF_demo/book_drf/modelviewset_view.py
@@ -1,5 +1,5 @@
 # ModelViewSet视图集的使用
-
+from rest_framework.decorators import action  # 要自动生成自定义方法的路由，需要使用action装饰器
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 
@@ -11,3 +11,9 @@ class Books(ModelViewSet):
     queryset = BookInfo.objects.all()  # 指定当前类视图使用的查询集数据
     serializer_class = BookSerializer  # 指定当前视图使用的序列化器
 
+    # 自定义方法
+    @action(methods=['get'], detail=True)  # methods:指定自定义方法对应的请求方法，detail:路由上如果要带pk参数，则使用detail=True将自动生成带pk参数的路由地址
+    def lastdata(self, request, pk):
+        book = BookInfo.objects.get(id=pk)
+        ser = BookSerializer(book)
+        return Response(ser.data)
